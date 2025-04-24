9297093efbdc937d99d98fbe671ad9ea6c51e852
Romario-Negreiros <nromario482@gmail.com>
2025-04-24 17:06:03 -0300

    Configure prisma

---
+++ prisma/schema/migrations/20250424161059_create_role_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "../prisma/generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.role.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
