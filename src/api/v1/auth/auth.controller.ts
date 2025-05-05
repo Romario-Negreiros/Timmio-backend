83984dee3b3a14f626efc3d72654dbd9294f5c9d
Romario-Negreiros <nromario482@gmail.com>
2025-05-05 14:10:35 -0300

    Configure swagger docs

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiTags, ApiOperation } from "@nestjs/swagger";
 
 import { AuthService } from "./auth.service";
 
+@ApiTags("Auth")
 @Controller({ path: "auth", version: "1" })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get()
+  @ApiOperation({ summary: "Teste" })
   getHello(): string {
     return this.authService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { VersioningType } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  // #region App config
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
@@ -16,6 +18,19 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  //#endregion
+
+  // #region Swagger config
+  const config = new DocumentBuilder()
+    .setTitle("Timmio API - V1")
+    .setDescription("Documentação e ambiente para testes da API")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${apiUrlPrefix}/v1/docs`, app, document);
+  //#endregion
 
   await app.listen(port);
 }
